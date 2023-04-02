b6cbdb56db177bac731322ce6911b2f48bd7432c
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-04-02 18:48:44 -0300

    terminado de funciones punto 9

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ To clone and run this application, you'll need Git and ghci installed on your co
 
 - `git clone https://github.com/LucasiBai/Lab-Practico2-FAMAF.git` to download the code.
 - `cd Lab-Practico2-FAMAF` go into the repository.
-- `ghci practico1.hs`
+- `ghci practico2.hs`
 
 ## Tools:
 
--- practico2.hs
+++ practico2.hs
@@ -114,3 +114,45 @@ customDrop n (x:xs) = customDrop (n-1) xs
 customConcat :: [a] -> [a] -> [a]
 customConcat [] ys = ys
 customConcat (x:xs) ys = x : customConcat xs ys
+
+--9)a) Función Fold
+maximo :: [Int] -> Int
+maximo [x] = x
+maximo (x:xs) = max x (maximo xs)
+
+--9)b) Función Fold
+sumaPares :: [(Int, Int)] -> Int
+sumaPares [] = 0
+sumaPares ((x,y):xs) = x + y + sumaPares xs
+
+--9)c) Función Fold
+todos0y1 :: [Int] -> Bool
+todos0y1 [] = True
+todos0y1 (x:xs) | x /= 1 && x /= 0 = False
+                | otherwise = True && todos0y1 xs
+
+--9)d) Función Filter
+quitar0s :: [Int] -> [Int]
+quitar0s [] = []
+quitar0s (x:xs) | x == 0 = quitar0s xs
+                | otherwise = x : quitar0s xs
+
+--9)e) Función Fold
+ultimo :: [a] -> a
+ultimo [x] = x
+ultimo (x:xs) = ultimo xs
+
+--9)f)
+repetir :: Int -> Int -> [Int]
+repetir 0 _ = []
+repetir n k = k : repetir (n-1) k
+
+--9)g) Función Fold
+concate :: [[a]] -> [a]
+concate [] = []
+concate (x:xs) = x ++ (concate xs)
+
+--9)h) Función Map
+rev :: [a] -> [a]
+rev [] = []
+rev (x:xs) = rev xs ++ [x]
